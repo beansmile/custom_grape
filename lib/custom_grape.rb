cb54963117699f8738826fc2be0b378b0e835670
MC <mc@beansmile.com>
2020-11-11 09:55:07 +0800

    first commit

---
+++ lib/custom_grape.rb
@@ -0,0 +1,6 @@
+require "custom_grape/version"
+
+module CustomGrape
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/custom_grape/version.rb
@@ -0,0 +1,3 @@
+module CustomGrape
+  VERSION = "0.1.0"
+end
