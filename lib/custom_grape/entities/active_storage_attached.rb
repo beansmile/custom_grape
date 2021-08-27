964d01bc47fe1ef8b7211e46a428c513d7f512c5
MC <mc@beansmile.com>
2021-08-27 16:07:12 +0800

    feat: Add documentation to active storage attached

--- lib/custom_grape/entities/active_storage_attached.rb
+++ lib/custom_grape/entities/active_storage_attached.rb
@@ -2,10 +2,10 @@ require "grape-entity"
 
 module CustomGrape::Entities
   class ActiveStorageAttached < Grape::Entity
-    expose :service_url, as: :url
-    expose :signed_id
-    expose :content_type
-    expose :filename
-    expose :byte_size
+    expose :service_url, as: :url, documentation: { desc: "Url" }
+    expose :signed_id, documentation: { desc: "Signed ID" }
+    expose :content_type, documentation: { desc: "Content type" }
+    expose :filename, documentation: { desc: "Filename" }
+    expose :byte_size, documentation: { desc: "Byte size" }
   end
 end
